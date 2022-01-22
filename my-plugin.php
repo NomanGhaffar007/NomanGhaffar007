545709ba8e3f5e8adaa9a52b2adb8a55e8e3d0e5
cubixsol <api.cubixsol@gmail.com>
2022-01-22 22:28:33 +0500

    First commit

---
+++ my-plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: Guesty v2 (vector)
+ * Description: Manage your real estate business and market your portfolio
+ * Version: 2.0.1
+ * Author: CubixSol
+ * Author URI: https://cubixsol.com
+ * Text Domain: guesty
+ */
